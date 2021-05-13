8569b38e8b6e8ea82c9b01fb0fceeb0178480ba7
tmtorr <tane@ualberta.ca>
2021-05-13 14:32:20 -0600

    Added simple functions

--- src_JS_to_PSQL/JS_to_PSQL_interface.js
+++ src_JS_to_PSQL/JS_to_PSQL_interface.js
@@ -1,3 +1,5 @@
+//TODO: turn the chunks of code into functions that can be called on the front end!
+
 const http = require('http');
 const { Sequelize, DataTypes, QueryInterface } = require('sequelize');
 
@@ -66,6 +68,7 @@ queryInterface.createTable('gene_expression', {
 let column_n = 4;
 //Loops through a specified number of times, likely a number which is the length of strings in an array
 //pulled from the columns of a csv file
+//This uses a simple numbering system
 for (let i = 0; i < (column_n + 1); i++){
     queryInterface.addColumn('gene_expression', `gene${i}`, {type: DataTypes.STRING });
 }
@@ -75,11 +78,6 @@ for (let i = 0; i < (column_n + 1); i++){
   queryInterface.removeColumn('gene_expression', `gene${i}`, {});
 }
 
-//Trying to deletes all columns in gene_expression using this, but isn't quite working yet
-/*
-queryInterface.bulkDelete('gene_expression', null, {});     
-*/
-
 //deletes all tables !!!CAREFUL!!! 
 /*
 queryInterface.dropAllTables(cell_db, {})
@@ -87,10 +85,60 @@ queryInterface.dropAllTables(cell_db, {})
 
 queryInterface.renameTable('gene_expression', 'gene_expression_and_ID', {});
 
-//
-//TODO: 
-//
-// - Look into creating an array/list that carries all organized column names
-// - Pull from the list using a for loop
-// - Use the previous method to populate the cell_store database!
-//
+
+//test array that contains gene expressions 
+var gene_expressions = ["SOD1","BEX1","ID4","XR"];
+
+//Loops through gene_expression names and populates them into a table... with specific names
+let fLen = gene_expressions.length;
+for (let i = 0; i < fLen; i++) {
+  queryInterface.addColumn('gene_expression_and_ID', gene_expressions[i], {type: DataTypes.STRING });
+}
+
+//Delete tables
+queryInterface.dropTable('gene_expression_and_ID', {});
+
+//function to rename a table
+function renameATable(original_name, new_name) {
+  queryInterface.renameTable(original_name, new_name, {});
+}
+
+//function to add columns using a numbered system
+function addNamedColumns(table_name, array_of_columns){
+  let fLen = array_of_columns.length;
+  for (let i = 0; i < fLen; i++) {
+    queryInterface.addColumn(table_name, array_of_columns[i], {type: DataTypes.STRING });
+  }
+}
+
+//deletes an array of columns that are passed in
+function deleteNamedColumns(table_name, array_of_columns ){
+  for (let i = 0; i < (column_n + 1); i++){
+    queryInterface.removeColumn(table_name, array_of_columns[i], {});
+  }
+}
+
+//deletes a specified column
+function deleteAColumn(table_name, column_to_drop){
+  queryInterface.removeColumn(table_name, column_to_drop, {});
+}
+
+//creates a table in this name
+function createATable(table_name){
+  queryInterface.createTable(table_name, {
+    name: DataTypes.STRING,
+    isBetaMember: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+      allowNull: false
+    }
+  });
+}
+
+/*
+//populates a table with specified data bitsies
+function populateColumn(table_name, column_name, inserted_data){
+  db.sequelize.query("INSERT INTO")
+}
+*/
+//NEEDS TO BE FIXED SO IT PROPERLY POPULATES THE TABLE
