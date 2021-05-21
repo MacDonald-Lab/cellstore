b5b754dc8c49ec73b4f18a91752416b377d89e27
Rajan Maghera <rmaghera@ualberta.ca>
2021-05-21 03:10:52 -0600

    Begin move of settings to file

---
+++ api/definitions/settings.js
@@ -0,0 +1,16 @@
+const settings = [
+    {
+        name: "organizationName",
+        friendlyName: "Organization Name",
+        type: "string",
+        protected: false
+    },
+    {
+        name: "adminUser",
+        friendlyName: "Administrator Username",
+        type: "string",
+        protected: true
+    }
+]
+
+export default settings
