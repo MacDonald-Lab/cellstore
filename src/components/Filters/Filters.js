e884631f8782571726a0c65bf3bdab1aeffb9bb4
Rajan Maghera <rmaghera@ualberta.ca>
2021-05-25 02:14:11 -0600

    Add multiselect checkbox filter using sets

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -2,6 +2,12 @@ import { Search16, Erase16 } from '@carbon/icons-react';
 import { Button, ButtonSet, Checkbox, Tile, TextInput, Dropdown } from 'carbon-components-react';
 import { React, useState } from 'react';
 
+function useForceUpdate() {
+    // eslint-disable-next-line
+    const [value, setValue] = useState(0); // integer state
+    return () => setValue(value => value + 1); // update the state to force render
+}
+
 const Filters = ({ library }) => {
 
     const FILTER_OPTIONS = {
@@ -78,14 +84,38 @@ const Filters = ({ library }) => {
         ]
     }
 
-    const defaultFilter = library.fields.map(({ name, dataType }) => ({
-        name: name,
-        dataType: dataType,
-        filter: null
-    }))
+    const defaultFilter = library.fields.map(({ name, dataType }) => {
+        
+        var filterValue = null
 
+        if (dataType === 'multiselect') filterValue = new Set()
+        
+        return {
+            name: name,
+            dataType: dataType,
+            filter: filterValue
+        }
+
+    })
+
+    const forceUpdate = useForceUpdate()
     const [filters, setFilters] = useState(defaultFilter)
 
+    const handleCheckbox = (value, id, event) => {
+        const fieldId = parseInt(id.split('-')[0])
+        const optionId = parseInt(id.split('-')[1])
+
+        if (value) {
+            filters[fieldId]['filter'].add(library.fields[fieldId]['multiselectOptions'][optionId]['storedAs'])
+        } else {
+            filters[fieldId]['filter'].delete(library.fields[fieldId]['multiselectOptions'][optionId]['storedAs'])
+        }
+
+        console.log(filters[fieldId]['filter'].has())
+        console.log(filters)
+        setFilters(filters)
+        forceUpdate()
+    }
 
     return (
         <Tile className="filters__main-tile">
@@ -108,8 +138,10 @@ const Filters = ({ library }) => {
                 </>
                 if (dataType === 'multiselect') return <>
                     {header}
-                    {item.multiselectOptions.map(({ friendlyName: optionName }, j) =>
-                        <Checkbox labelText={optionName} id={`${i}-${j}`} key={`${i}-${j}`} />
+                    {item.multiselectOptions.map(({ friendlyName: optionName, storedAs }, j) =>
+
+                    
+                        <Checkbox labelText={optionName} id={`${i}-${j}`} key={`${i}-${j}`} checked={filters[i]['filter'].has(storedAs)} onChange={handleCheckbox} />
                     )}
                     <br />
                 </>
