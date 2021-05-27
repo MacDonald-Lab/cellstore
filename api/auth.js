5c0198e6d4937b71d9e868f36c919956d3e417fd
tmtorr <tane@ualberta.ca>
2021-05-27 15:15:03 -0600

    Changed hashing string

--- api/auth.js
+++ api/auth.js
@@ -47,7 +47,7 @@ app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(flash())
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: 'super_secret_passcode',
   resave: false,
   saveUninitialized: false
 }))
