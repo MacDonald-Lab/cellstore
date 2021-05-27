a000944480b25cf9abd9ce9c20d57f83557277c9
tmtorr <tane@ualberta.ca>
2021-05-27 15:02:16 -0600

    Added working authentication pages

--- api/auth.js
+++ api/auth.js
@@ -2,13 +2,26 @@
 // dotenv.config()
 
 import express from 'express'
-//import passport from 'passport'
+const app = express();
+
+import bcrypt from 'bcrypt'
+import passport from 'passport'
+import flash from 'express-flash'
+import session from 'express-session'
+import methodOverride from 'method-override'
 //import cookieParser from 'cookie-parser'
 //import session from 'express-session'
 //import connectMultiparty from 'connect-multiparty'
 //import User from './user.js'
 
-const app = express();
+const initializePassport = require('./passport-config')
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
+//only in memory, so we'll want to put this into a database
+const users = []
 
 
 // app.use(express.urlencoded({ extended: true }));
@@ -29,10 +42,70 @@ const app = express();
 // passport.deserializeUser(User.deserializeUser());
 
 
-app.set('view-engine', 'ejs')
 
-app.get('/', (req, res) => {
-   res.render('authIndex.ejs')
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+app.get('/', checkAuthenticated, (req, res) => {
+  res.render('authIndex.ejs', { name: req.user.name })
 })
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+  res.render('login.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render('register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
+  }
+})
+
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
 
 app.listen(4000)
