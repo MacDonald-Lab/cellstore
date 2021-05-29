3f123fd1d41e670e62c39ee5b159412510ca4d8b
Rajan Maghera <rmaghera@ualberta.ca>
2021-05-28 23:49:08 -0600

    Added commented out pyodide (for Python in JS)

--- src/content/ComputationPage/ComputationPage.js
+++ src/content/ComputationPage/ComputationPage.js
@@ -1,10 +1,25 @@
 import { Button, Column, Grid, Row } from 'carbon-components-react'
 import { React } from 'react'
-import {useHistory} from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
+// import HEKAReaderPy from './HEKAReaderPy'
+
+// const handlePython = async () => {
+//     console.log(window.pyodide.runPythonAsync(HEKAReaderPy));
+// }
 
 const ComputationPage = () => {
-    
+
     const history = useHistory()
+
+    // useEffect(() => {
+    //     const loadPyodide = async () => {
+    //         await window.loadPyodide({
+    //             indexURL : process.env.PUBLIC_URL + "/pyodide/"
+    //         })
+    //     }
+
+    //     loadPyodide()
+    // }, [])
 
     return <Grid>
         <Row>
@@ -19,6 +34,12 @@ const ComputationPage = () => {
                 <Button onClick={() => history.push('/computations/image-test')}>Go to Image Classification Test</Button>
             </Column>
         </Row>
+        {/* <Row>
+            <Column>
+                <h1>Python Code Test</h1>
+                <Button onClick={() => handlePython()}>Run Program</Button>
+            </Column>
+        </Row> */}
     </Grid>
 }
 
