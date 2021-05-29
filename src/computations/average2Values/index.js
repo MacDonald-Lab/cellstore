5f83bad3eb112a84b4420fb6c6854fce6bd43c83
Rajan Maghera <rmaghera@ualberta.ca>
2021-05-28 23:42:44 -0600

    Create basic computation definition

---
+++ src/computations/average2Values/index.js
@@ -0,0 +1,46 @@
+const definition = {
+    name: "average-2-values",
+    friendlyName: "Average 2 Values",
+    description: "Determine the mean (average) of 2 values in 2 columns",
+
+    inputs: [
+        {
+            name: "value-a",
+            friendlyName: "Value A",
+            parameterName: "valueA",
+            description: "The first value",
+            dataTypes: ["int", "float"],
+            required: true
+
+        },
+        {
+            name: "value-b",
+            friendlyName: "Value B",
+            parameterName: "valueB",
+            description: "The second value",
+            dataTypes: ["int", "float"],
+            required: true
+        },
+    ],
+
+    outputs: [
+        {
+            name: "average",
+            friendlyName: "Average",
+            parameterName: "average",
+            description: "The mean of value A and B",
+            dataType: "float"
+        }
+    ]
+}
+
+const averageValue = ({ valueA, valueB }) => {
+    return (valueA + valueB)/2
+}
+
+const exportObject = {
+    definition: definition,
+    function: averageValue
+}
+
+export default exportObject
