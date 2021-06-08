38ce38772f0d751c9092a48f3b7310b2d7d4462b
Rajan Maghera <rmaghera@ualberta.ca>
2021-06-07 18:29:29 -0600

    Update user provider

--- src/contexts/UserProvider.jsx
+++ src/contexts/UserProvider.jsx
@@ -1,16 +1,26 @@
-import React, { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
+import {useHistory} from "react-router-dom"
 const context = createContext(null);
 
 const UserProvider = ({ children }) => {
   const [user, setUser] = useState({});
+  const history = useHistory()
 
   useEffect(() => {
-      fetch("/api/auth/user")
-          .then(res => res.json())
-          .then(res => setUser(res))
-          .catch(err => {
-              console.log(err);
-          });
+
+    const fetchData = async () => {
+
+         const response = await fetch("/api/auth/user")
+         
+         if (response.status === 401) {
+             return history.push('/login')
+         } else {
+             const toJson = await response.json()
+             setUser(toJson)
+         }
+    }
+
+    fetchData()
   }, []);
 
   return (
