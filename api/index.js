a3ff6562a38e4ab801230659dd4cb2c19412eb71
Rajan Maghera <rmaghera@ualberta.ca>
2021-06-08 18:01:18 -0600

    Add static hosting to express server

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config()
 
+import path from 'path'
+
 import express from 'express';
 import seq from 'sequelize';
 const { DataTypes } = seq;
@@ -62,6 +64,16 @@ app.use(cookieSession({
 
 app.use('/api/auth', authRoutes(sequelize, app))
 app.use('/api/v1', apiRoutes(sequelize, models))
+
+if (process.env.NODE_ENV === 'production') {
+
+  const __dirname = path.resolve()
+    app.use(express.static(path.join(__dirname, 'build')))
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'build/index.html'))
+    })
+
+}
 
 app.listen(port, () => {
   console.log(`Development API listening on port ${port}`)
