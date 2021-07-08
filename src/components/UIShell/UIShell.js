76a48e87d1793a12e7a7ec01071a290cf9fe5535
Rajan Maghera <rmaghera@ualberta.ca>
2021-07-07 23:51:26 -0600

    Move UI shell to external css

--- src/components/UIShell/UIShell.js
+++ src/components/UIShell/UIShell.js
@@ -1,5 +1,5 @@
-import { useContext } from 'react';
-import UserContext from '../../contexts/UserProvider'
+import { useContext } from "react";
+import UserContext from "../../contexts/UserProvider";
 // import Electron from 'electron'
 
 import {
@@ -16,42 +16,67 @@ import {
   SideNav,
   SideNavItems,
   HeaderSideNavItems,
-} from 'carbon-components-react';
+} from "carbon-components-react";
 
-import { Link, useLocation, useHistory } from 'react-router-dom'
+import { Link, useLocation, useHistory } from "react-router-dom";
 
 import {
   Settings20,
   Notification20,
   UserAvatar20,
-  Add16
-} from '@carbon/icons-react';
+  Add16,
+} from "@carbon/icons-react";
 
 // TODO: Loop header items and make isCurrentPage dynamic
 // FIXME: iscurrentpage styling on sidebar
 
 const UIShell = ({ organizationName, libraries }) => {
-  const location = useLocation()
-  const history = useHistory()
+  const location = useLocation();
+  const history = useHistory();
 
-  const user = useContext(UserContext.context)
-var userAgent = navigator.userAgent.toLowerCase()
+  const user = useContext(UserContext.context);
+  var userAgent = navigator.userAgent.toLowerCase();
 
-  const MenuItems = (props) => <>
-    <HeaderMenu aria-label="Link 4" menuLinkName="Libraries" isCurrentPage={location.pathname === '/library'}>
-      {libraries.map(item =>
-
-        <HeaderMenuItem key={item.name} element={Link} to={"/library/" + item.name} onClick={props.onClick}>{item.friendlyName}</HeaderMenuItem>
-      )}
-      <HeaderMenuItem element={Link} to="/settings/create" onClick={props.onClick}><Add16 />Create library</HeaderMenuItem>
-    </HeaderMenu>
-    <HeaderMenuItem isCurrentPage={location.pathname === '/computations'} element={Link} to="/computations" onClick={props.onClick}>Computations</HeaderMenuItem>
-
-  </>
+  const MenuItems = (props) => (
+    <>
+      <HeaderMenu
+        aria-label="Link 4"
+        menuLinkName="Libraries"
+        isCurrentPage={location.pathname === "/library"}
+      >
+        {libraries.map((item) => (
+          <HeaderMenuItem
+            key={item.name}
+            element={Link}
+            to={"/library/" + item.name}
+            onClick={props.onClick}
+          >
+            {item.friendlyName}
+          </HeaderMenuItem>
+        ))}
+        <HeaderMenuItem
+          element={Link}
+          to="/settings/create"
+          onClick={props.onClick}
+        >
+          <Add16 />
+          Create library
+        </HeaderMenuItem>
+      </HeaderMenu>
+      <HeaderMenuItem
+        isCurrentPage={location.pathname === "/computations"}
+        element={Link}
+        to="/computations"
+        onClick={props.onClick}
+      >
+        Computations
+      </HeaderMenuItem>
+    </>
+  );
 
   return (
     <HeaderContainer
-      render={({ isSideNavExpanded, onClickSideNavExpand }) =>
+      render={({ isSideNavExpanded, onClickSideNavExpand }) => (
         <Header aria-label="CellSTORE Header">
           <SkipToContent />
           <HeaderMenuButton
@@ -59,36 +84,37 @@ var userAgent = navigator.userAgent.toLowerCase()
             onClick={onClickSideNavExpand}
             isActive={isSideNavExpanded}
           />
-          <HeaderName element={Link} to="/" prefix={organizationName}>CellSTORE</HeaderName>
+          <HeaderName element={Link} to="/" prefix={organizationName}>
+            CellSTORE
+          </HeaderName>
           <HeaderNavigation aria-label="CellSTORE Header">
             <MenuItems />
           </HeaderNavigation>
-          <SideNav aria-label="Side navigation" expanded={isSideNavExpanded} isPersistent={false}>
+          <SideNav
+            aria-label="Side navigation"
+            expanded={isSideNavExpanded}
+            isPersistent={false}
+          >
             <SideNavItems>
               <HeaderSideNavItems>
                 <MenuItems onClick={onClickSideNavExpand} />
               </HeaderSideNavItems>
             </SideNavItems>
           </SideNav>
-          {userAgent.indexOf(' electron/') > -1 &&
-
-            <div style={{ backgroundColor: 'red', width: '50%', height: '100%', margin: 'auto' }}
-              className='bx--header__draggable' >
-            </div>
-          }
-
+          {userAgent.indexOf(" electron/") > -1 && (
+            <div className="bx--header__draggable ui-shell__electron-draggable"></div>
+          )}
 
           <HeaderGlobalBar>
-            <HeaderGlobalAction aria-label="Log out"
-              style={{ width: 'auto', paddingRight: 10, paddingLeft: 10 }}
+            <HeaderGlobalAction
+              aria-label="Log out"
+              className="ui-shell__logout-button"
               onClick={() => {
-                fetch('/api/auth/logout')
-                history.push('/login')
-
+                fetch("/api/auth/logout");
+                history.push("/login");
               }}
             >
-
-              Log out of <span style={{ fontWeight: 'bold' }}>{user.username}</span>
+              Log out of {user.username}
             </HeaderGlobalAction>
             <HeaderGlobalAction aria-label="Notifications">
               <Notification20 />
@@ -96,22 +122,25 @@ var userAgent = navigator.userAgent.toLowerCase()
             <HeaderGlobalAction aria-label="User Avatar">
               <UserAvatar20 />
             </HeaderGlobalAction>
-            <HeaderGlobalAction aria-label="Settings" onClick={() => history.push('/settings')}>
+            <HeaderGlobalAction
+              aria-label="Settings"
+              onClick={() => history.push("/settings")}
+            >
               <Settings20 />
             </HeaderGlobalAction>
 
-          {userAgent.indexOf(' electron/') > -1 &&
-            <HeaderGlobalAction aria-label="Minimize" onClick={() => console.log('minimize')}>
-              <Settings20 />
-            </HeaderGlobalAction>
-
-
-            }
-
+            {userAgent.indexOf(" electron/") > -1 && (
+              <HeaderGlobalAction
+                aria-label="Minimize"
+                onClick={() => console.log("minimize")}
+              >
+                <Settings20 />
+              </HeaderGlobalAction>
+            )}
           </HeaderGlobalBar>
         </Header>
-      }
+      )}
     />
-  )
+  );
 };
 export default UIShell;
