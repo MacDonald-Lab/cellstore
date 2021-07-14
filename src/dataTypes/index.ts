b97827ee6a4306f8f5a57e667abb8705aef8e6a1
Rajan Maghera <rmaghera@ualberta.ca>
2021-07-13 23:48:13 -0600

    Update total data type routes

--- src/dataTypes/index.ts
+++ src/dataTypes/index.ts
@@ -1,34 +1,54 @@
 // import seq from 'sequelize'
 
-import GeneExpression from './GeneExpression'
+import GeneExpression from "./GeneExpression";
 
-const types = [
-    GeneExpression
-]
+const types = [GeneExpression];
 
-const initDescriptions = () => types.map(type => type.initDescription())
+const initDescriptions = () => types.map((type) => type.initDescription());
 
-const initViews = (libraryName: string, queryData: JSON) => {
-    const views = []
-    for (var type in types) {
-        const view = types[type].initView(libraryName, queryData)
-        if (view) views.push(view)
-    }
-    return views
-}  
+const initViews = (typeData: { [any: string]: any }) => {
+  var views = [];
+  for (const typeName of Object.keys(typeData)) {
+    const type = types.find((value) => (value.initDescription().id = typeName));
+    if (!type) continue;
+    views.push(type.initView(typeData[typeName]));
+  }
+  return views;
+};
 
 const getDatabaseDefinition = (typeName: string) => {
-    const type = types.find(value => value.initDescription().id = typeName)
-    if (!type) return
-    return type.getDatabaseDefinition
-}
+  const type = types.find((value) => (value.initDescription().id = typeName));
+  if (!type) return;
+  return type.getDatabaseDefinition;
+};
+
+const getDefinition = (typeName: string) => {
+  const type = types.find((value) => (value.initDescription().id = typeName));
+  if (!type) return;
+  return type.initDescription();
+};
+
+const getOnAdd = (typeName: string) => {
+  const type = types.find((value) => (value.initDescription().id = typeName));
+  if (!type) return;
+  return type.onAdd;
+};
+
+const getType = (typeName: string) => {
+  const type = types.find((value) => (value.initDescription().id = typeName));
+  if (!type) return;
+  return type;
+};
 
 const moduleExports = {
-    initViews,
-    initDescriptions,
-    getDatabaseDefinition,
+  initViews,
+  initDescriptions,
+  getDatabaseDefinition,
+  getDefinition,
+  getOnAdd,
+  getType,
 
-    GeneExpression
-}
+  GeneExpression,
+};
 
-export default moduleExports
+export default moduleExports;
